7989a49f59bf73a1940fc3d31e0690941857c9f6
Javier Julio <jjfutbol@gmail.com>
2014-10-08 00:55:18 -0400

    Generate dictionary.json file and a test json set

--- parse.rb
+++ parse.rb
@@ -6,23 +6,36 @@ files = Dir.glob("sources/gcide/gcide_[a-z]-entries.xml")
 
 word_count = 0
 definition_count = 0
+result = {}
 
 files.each do |file|
   
   puts "Parsing #{File.basename(file)}"
   
-  result = Parser.new(File.read(file)).parse
+  result.merge! Parser.new(File.read(file)).parse
   
-  word_count += result.keys.count
-  definition_count += result.values.inject(0) { |sum, hash| sum + hash[:definitions].count }
-  
-  new_file_name = File.basename(file, ".*")
-  
-  File.open("#{new_file_name}.json", "w") do |f|
-    f.write(JSON.pretty_generate(JSON.parse(result.to_json)))
-  end
-
 end
+
+File.open("dictionary.json", "w") do |f|
+  f.write(JSON.pretty_generate(JSON.parse(result.to_json)))
+end
+
+word_count += result.keys.count
+definition_count += result.values.inject(0) { |sum, hash| sum + hash[:definitions].count }
 
 puts "Words: #{word_count}"
 puts "Definitions: #{definition_count}"
+
+
+
+smaller_data_set = {
+  artifact: result["artifact"],
+  homœomeria: result["homœomeria"],
+  deduction: result["deduction"],
+  reduction: result["reduction"],
+  aëneous: result["aëneous"]
+}
+
+File.open("dictionary-test.json", "w") do |f|
+  f.write(JSON.pretty_generate(JSON.parse(smaller_data_set.to_json)))
+end
