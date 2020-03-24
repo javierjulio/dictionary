c17de6606b546864425fa2bf51d8938664b803c7
Javier Julio <jjfutbol@gmail.com>
2020-03-24 11:11:50 -0400

    Update test helper to use spec minitetst-reporters
    Also updates template to match what gets generated by new gems by bundler.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,10 @@
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require 'parser'
 require 'minitest/autorun'
+require 'minitest/reporters'
 require 'shoulda/context'
+
+Minitest::Reporters.use!([Minitest::Reporters::SpecReporter.new])
 
 def load_fixture(file_name)
   path = File.expand_path("../fixtures/#{file_name}", __FILE__)
